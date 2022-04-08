00361b74fefc16cbff1997a74c4c4ab205a99230
Matias Diaz Damian <diazmatiasdamian@gmail.com>
2022-04-08 19:09:37 -0300

    Subiendo nuestro repositorio a github

---
+++ webpack.config.dev.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+   entry: './src/index.js', //El archivo fuente que se va ir a produccion
+   output: {
+       path: path.resolve(__dirname, 'dist'),
+       filename: "bundle.js",
+   },
+   resolve: {
+       extensions: ['.js', '.jsx']
+   },
+   modo: 'development',
+   module: {
+      rules: [
+          {
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+            }
+          },
+          {
+              test: /\.html$/,
+              use: [
+                  {loader: 'html-loader'}
+              ]
+          },
+          {
+              test: /\.s[ac]ss$/,
+              use: [
+                  'style-loader',
+                  'css-loader',
+                  'sass-loader'
+              ]
+          }
+      ] 
+   },
+   plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+            filename: './index.html'
+        }),
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        })
+   ],
+   devServer: {
+       static: {
+           directory: path.join(__dirname, 'dist'),
+       },
+       compress: true,
+       port: 3006,
+       open: true,   //Nos permite abrir el navegador para ver cambios en tiempo real  
+   }
+
+}
